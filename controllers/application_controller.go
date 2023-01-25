4e574dedf6c2e5ad93b71a8d920c0b1b9d9b655b
冯浩 <fenghao@xiaoduotech.com>
2023-01-25 19:31:16 +0800

    给role添加额外的deployment和service权限

--- controllers/application_controller.go
+++ controllers/application_controller.go
@@ -48,6 +48,11 @@ type ApplicationReconciler struct {
 //+kubebuilder:rbac:groups=apps.isadba.com,resources=applications,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=apps.isadba.com,resources=applications/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=apps.isadba.com,resources=applications/finalizers,verbs=update
+// 添加额外的role权限
+//+kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=apps,resources=deployments/status,verbs=get
+//+kubebuilder:rbac:groups=core,resources=services,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=core,resources=services/status,verbs=get
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
